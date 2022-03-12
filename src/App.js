e0e0e0d5ba1b6330b3820424ee9e8600599455d2
chrisphum <76226574+chrisphum@users.noreply.github.com>
2022-03-11 20:41:16 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,43 @@ import Table2 from "./Table";
 import Search from "./Search"
 import DrawerComp from "./DrawerComp"
 
+
+
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //
+    //
+    // Hello! 
+    // Please checkout my React Native iPhone App!
+    // It is not complete, but it is a better showcase of my skills.
+    // Even if it is (currently) a little unorganized 
+    // ( it could defintely use more organized data structures)
+    // Soon I will apply to the Apple App Store
+
+    // Github link: https://github.com/chrisphum/iPhoneAppForAlkira
+    // Youtube demonstration: https://www.youtube.com/watch?v=1ft7Od9pDUw 
+    //
+    // Please also read the Readme.md on the github repo! Thanks for the 
+    // chance to interview at Alkira!
+    //
+    //
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var list = []
 
 export const DataContext = React.createContext();
